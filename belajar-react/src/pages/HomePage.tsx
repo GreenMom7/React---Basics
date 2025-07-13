180b0f450ecc67ec9926c00eb1866cdcf833f7e9
Akmal <akmalsabri73@gmail.com>
2025-07-13 13:24:47 +0800

    create pages

---
+++ belajar-react/src/pages/HomePage.tsx
@@ -0,0 +1,12 @@
+import Like from "../components/Like";
+
+const HomePage = () => {
+  return (
+    <>
+      <h1>Welcome to React JS</h1>
+      <Like />
+    </>
+  );
+};
+
+export default HomePage;
---
+++ belajar-react/src/pages/PlayPage.tsx
@@ -0,0 +1,11 @@
+import Counter from "../components/Counter";
+
+const PlayPage = () => {
+  return (
+    <>
+      <Counter />
+    </>
+  );
+};
+
+export default PlayPage;
