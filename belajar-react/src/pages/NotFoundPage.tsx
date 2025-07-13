288d64ecea2f47e2ffa91623967c2f007aeadea7
Akmal <akmalsabri73@gmail.com>
2025-07-13 17:05:21 +0800

    update notFound page style

--- belajar-react/src/pages/NotFoundPage.tsx
+++ belajar-react/src/pages/NotFoundPage.tsx
@@ -2,7 +2,9 @@ const NotFoundPage = () => {
   return (
     <>
       <div>
-        <h1>404 Page Not Found</h1>
+        <h1 className="text-4xl text-gray-950 tracking-tighter text-balance">
+          404 Page Not Found
+        </h1>
       </div>
     </>
   );
