3f6a74c746baac2018fd8a0e250c3202d9f563a9
Akmal <akmalsabri73@gmail.com>
2025-07-13 13:58:11 +0800

    Linking pages

--- belajar-react/src/pages/HomePage.tsx
+++ belajar-react/src/pages/HomePage.tsx
@@ -1,11 +1,17 @@
+import { Link } from "react-router";
 import Like from "../components/Like";
+import { Button } from "../components/ui/button";
 
 const HomePage = () => {
   return (
-    <>
-      <h1>Welcome to React JS</h1>
+    <div>
+      <h1 className="mb-5">Welcome to React JS</h1>
       <Like />
-    </>
+
+      <Button className="m-2" variant="link" size="lg">
+        <Link to="/play">Play?</Link>
+      </Button>
+    </div>
   );
 };
 
--- belajar-react/src/pages/PlayPage.tsx
+++ belajar-react/src/pages/PlayPage.tsx
@@ -1,9 +1,15 @@
+import { Link } from "react-router";
 import Counter from "../components/Counter";
+import { Button } from "../components/ui/button";
 
 const PlayPage = () => {
   return (
     <>
       <Counter />
+
+      <Button variant="link" size="lg">
+        <Link to="/">Home</Link>
+      </Button>
     </>
   );
 };
