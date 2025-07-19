8638812cc5dbdb5ce4a674378b95fe2dc00f3221
Akmal <akmalsabri73@gmail.com>
2025-07-20 00:08:36 +0800

    add show password toggle

--- belajar-react/src/pages/RFHPage.tsx
+++ belajar-react/src/pages/RFHPage.tsx
@@ -1,6 +1,7 @@
 import { useForm } from "react-hook-form";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { useState } from "react";
 // type RegisterFormSchema = {
 //   username: string;
 //   password: string;
@@ -9,12 +10,14 @@ import { zodResolver } from "@hookform/resolvers/zod";
 const registerFormSchema = z.object({
   username: z.string().min(3).max(10),
   password: z.string().min(8),
-  age: z.coerce.number().min(18),
+  age: z.coerce.number<number>().min(18),
 });
 
 type RegisterFormSchema = z.infer<typeof registerFormSchema>;
 
 const RFHPage = () => {
+  const [showPassword, setShowPassword] = useState(false);
+
   const form = useForm<RegisterFormSchema>({
     resolver: zodResolver(registerFormSchema),
   });
@@ -24,7 +27,7 @@ const RFHPage = () => {
     console.log(values);
   };
   return (
-    <div className="">
+    <div className="container">
       <h1>React Hook Form</h1>
 
       <form
@@ -37,12 +40,27 @@ const RFHPage = () => {
         <span style={{ color: "red" }}>
           {form.formState.errors.username?.message}
         </span>
+
         <label>
-          Passowrd : <input type="text" {...form.register("password")} />
+          Passowrd :{" "}
+          <input
+            type={showPassword ? "text" : "password"}
+            {...form.register("password")}
+          />
         </label>
         <span style={{ color: "red" }}>
           {form.formState.errors.password?.message}
         </span>
+
+        <label>
+          <input
+            type="checkbox"
+            onChange={(event) => setShowPassword(event.target.checked)}
+            style={{ marginRight: "1rem" }}
+          />
+          Show password
+        </label>
+
         <label>
           Age : <input type="number" {...form.register("age")} />
         </label>
